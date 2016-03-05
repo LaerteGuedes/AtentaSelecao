3cad35c3c15c59b0c17797138a31675b73ac37cb
LaerteGM <laerteguedes8@gmail.com>
2016-03-05 12:18:08 -0300

    Util

---
+++ src/main/java/atenta/util/Compras.java
@@ -0,0 +1,22 @@
+package atenta.util;
+
+import atenta.model.Compra;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+public class Compras {
+
+    private List<Compra> compraList;
+
+    @XmlElement(name="compra")
+    public List<Compra> getCompraList() {
+        return compraList;
+    }
+
+    public void setCompraList(List<Compra> compraList) {
+        this.compraList = compraList;
+    }
+}
---
+++ src/main/java/atenta/util/Produtos.java
@@ -0,0 +1,24 @@
+package atenta.util;
+
+import atenta.model.Produto;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement
+public class Produtos {
+
+    private List<Produto> produtoList;
+
+    @XmlElement(name = "produto")
+    public List<Produto> getProdutoList() {
+        return produtoList;
+    }
+
+    public void setProdutoList(List<Produto> produtoList) {
+        this.produtoList = produtoList;
+    }
+
+
+}
